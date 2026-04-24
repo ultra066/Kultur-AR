20408f24f7e994385690d8eeb0fb0a928d75fad3
ultra066 <hvultra@gmail.com>
2026-04-24 17:28:51 +0800

    chore: update package.json scripts and dependencies
    - Changed scripts for Android and iOS to use 'expo run' instead of 'expo start'.
    - Updated '@rnmapbox/maps' dependency version to '10.2.10'.
    - Updated 'expo-speech' dependency to version '~14.0.8'.

---
+++ app/frontend/homepage/utils.js
@@ -0,0 +1,8 @@
+export const debounce = (func, delay) => {
+  let timeoutId;
+  return (...args) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => func(...args), delay);
+  };
+};
+
